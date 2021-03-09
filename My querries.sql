0d44dc87f47de3d1780f1374afaf1e2ab3136528
KyleWalkerley <78598168+KyleWalkerley@users.noreply.github.com>
2021-03-09 08:53:57 +0200

    Add files via upload

---
+++ My querries.sql
@@ -0,0 +1,15 @@
+INSERT INTO animal (animal_name,animal_age,animal_color,animal_gender)
+VALUES ('Lion', 4,'Orange', 'Male'),
+('Tiger', 6,'Orange', 'Female'),
+('Bear', 2,'Black', 'Male'),
+('Snake', 8,'Grey', 'Male');
+
+
+3.1
+Numeric:Because you are collecting number data from the vehicles.
+
+3.2
+You can put it in as driver then first_name and last_name so you can view it easier.If you want to search someone by their first name and or last name
+
+3.3
+
---
+++ SQL Recources.sql
@@ -0,0 +1,49 @@
+CREATE TABLE number_data_types (
+	numeric_column numeric(20,5),
+	real_column real,
+	double_column double precision
+);
+
+ INSERT INTO number_data_types
+VALUES
+ (.7, .7, .7),
+ (2.13579, 2.13579, 2.13579),
+ (2.1357987654, 2.1357987654, 2.1357987654);
+SELECT * FROM number_data_types;
+
+
+SELECT
+numeric_column * 10000000 AS "Fixed",
+real_column * 10000000 AS "Float"
+
+FROM number_data_types
+
+WHERE numeric_column = .7;
+
+
+CREATE TABLE date_time_types (
+timestamp_column timestamp with time zone,
+interval_column interval
+);
+
+INSERT INTO date_time_types
+VALUES
+ ('2018-12-31 01:00 EST','2 days'),
+ ('2018-12-31 01:00 -8','1 month'),
+ ('2018-12-31 01:00 Australia/Melbourne','1 century'),
+ (now(),'1 week');
+ 
+SELECT * FROM date_time_types;
+
+
+SELECT timestamp_column, CAST(timestamp_column AS varchar(10))
+FROM date_time_types;
+
+SELECT numeric_column,
+ CAST(numeric_column AS integer),
+ CAST(numeric_column AS varchar(6))
+FROM number_data_types;
+
+
+
+
