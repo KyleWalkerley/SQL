9219ad5865b2b429e3990335668873d5867ba841
KyleWalkerley <78598168+KyleWalkerley@users.noreply.github.com>
2021-03-12 21:23:59 +0200

    Add files via upload

--- Employees_DB/Employees.sql
+++ Employees_DB/Employees.sql
@@ -5,9 +5,10 @@ last_name varchar(50),
 id_number varchar(20),
 age varchar(3),
 nationality varchar(50),
-date_of_birth date,
+date_of_birth integer,
 phone varchar(10),
-CONSTRAINT employee_key PRIMARY KEY (employee_id)
+CONSTRAINT employee_key PRIMARY KEY (employee_id),
+titles_id integer REFERENCES titles (titles_id)
 );
 
 CREATE TABLE departments(
@@ -17,14 +18,17 @@ CONSTRAINT dep_key PRIMARY KEY (dep_id)
 );
 
 CREATE TABLE salaries(
-salaries_id integer,
+salaries_id serial,
 salary numeric,
 CONSTRAINT salaries_key PRIMARY KEY (salaries_id)
 );
 
+drop table salaries;
+
 CREATE TABLE titles(
 titles_id bigserial,
 titles varchar(50),
+dep_id integer REFERENCES departments (dep_id),
 CONSTRAINT titles_key PRIMARY KEY (titles_id)	
 );
 
@@ -38,25 +42,35 @@ Total_hours_worked integer,
 CONSTRAINT overtime_hours_key PRIMARY KEY (overtime_hours_id)
 );
 
-alter table
+ALTER TABLE employees 
+add column department integer references departments(dep_id);
 
-Insert into employees(first_name,last_name,id_number,age ,nationality,date_of_birth,phone,departments,Titles)
+ALTER TABLE employees 
+add column salary integer references salaries(salaries_id);
+
+ALTER TABLE employees 
+add column title integer references titles(titles_id);
+
+ALTER TABLE employees 
+add column hours_worked integer references Overtime_Hours(overtime_hours_id);
+
+Insert into employees(first_name,last_name,id_number,age ,nationality,date_of_birth,phone,titles_id)
 Values
-('Kyle', 'Walkerley', '0110305061084', '19', 'South-Africa' 2001-10-30, '0769409564'),
-('Jan', 'Pan', '0104186452012', '19', 'South-Africa' 2001-04-18, '0845624789'),
-('Juan', 'Engelbreght', '0109047461325', '19', 'Zimbabwe' 2001-09-04, '0145789354'),
-('Richard', 'Nel', '0101013458741', '19', 'Botswana' 2001-01-01, '0348789652'),
-('Robert', 'Spence', '0105097845123', '19', 'Germany' 2001-05-09, '0789561425'),
-('Stehan', 'Wollmerans', '9906012457987', '21', 'America' 1999-06-01, '0548787451'),
-('Calvin', 'Smith', '0002013265987', '20', 'South-Africa' 2000-02-01, '0789456123'),
-('Armin', 'Jonker', '8812258469146', '33', 'Mexico' 1988-12-25, '0767845212'),
-('Ruan', 'Appelgryn', '0110205784107', '19', 'Japan' 2001-10-20, '0762563254'),
-('Estian', 'Beeslaar', '0112120201045', '19', 'South-Africa' 2001-12-12, '0778945612'),
-('Lucas', 'Schooman', '0111110245784', '19', 'South-Africa' 2001-11-11, '0789369854'),
-('Jean-Marie', 'Van der Colf', '0108045487952', '19', 'South-Africa' 2001-08-04, '0014587954'),
-('Marisca', 'Van der Wald', '0112310125478', '19', 'South-Africa' 2001-12-31, '0894561478');
+('Kyle', 'Walkerley', '0110305061084', '19', 'South-Africa', 2001-10-30, '0769409564',1),
+('Jan', 'Pan', '0104186452012', '19', 'South-Africa', 2001-04-18, '0845624789',2),
+('Juan', 'Engelbreght', '0109047461325', '19', 'Zimbabwe', 2001-09-04, '0145789354',3),
+('Richard', 'Nel', '0101013458741', '19', 'Botswana', 2001-01-01, '0348789652',4),
+('Robert', 'Spence', '0105097845123', '19', 'Germany', 2001-05-09, '0789561425',5),
+('Stehan', 'Wollmerans', '9906012457987', '21', 'America', 1999-06-01, '0548787451',6),
+('Calvin', 'Smith', '0002013265987', '20', 'South-Africa', 2000-02-01, '0789456123',7),
+('Armin', 'Jonker', '8812258469146', '33', 'Mexico', 1988-12-25, '0767845212',8),
+('Ruan', 'Appelgryn', '0110205784107', '19', 'Japan', 2001-10-20, '0762563254',9),
+('Estian', 'Beeslaar', '0112120201045', '19', 'South-Africa', 2001-12-12, '0778945612',10),
+('Lucas', 'Schooman', '0111110245784', '19', 'South-Africa', 2001-11-11, '0789369854',8),
+('Jean-Marie', 'Van der Colf', '0108045487952', '19', 'South-Africa', 2001-08-04, '0014587954',2),
+('Marisca', 'Van der Wald', '0112310125478', '19', 'South-Africa', 2001-12-31, '0894561478',5);
 
-insert into departments (dep_id)
+insert into departments (department_name)
 values
 ('HR'), 
 ('ENGINEERING'), 
@@ -69,39 +83,39 @@ values
 ('ACCOUNTING_FINANCES'), 
 ('MARKETING'); 
 
-insert into titles (title_id)
+insert into titles (titles,dep_id)
 values
-('Manager'),
-('Intern'),
-('Senior'),
-('Junior'),
-('Manager'),
-('Intern'),
-('Senior'),
-('Junior'),
-('Manager'),
-('Senior'),
-('Junior'),
-('Manager'),
-('Intern');
+('Manager',1),
+('Intern',2),
+('Senior',3),
+('Junior',4),
+('Manager',5),
+('Intern',6),
+('Senior',7),
+('Junior',8),
+('Manager',9),
+('Senior',10),
+('Junior',5),
+('Manager',10),
+('Intern',8);
 
-insert into salaries (salaries_id)
+insert into salaries(salary,salary_id)
 values
-(50000),
-(1000000),
-(250000),
-(350000),
-(650000),
-(190000),
-(495000),
-(345000),
-(2500000),
-(190000),
-(495000),
-(345000),
-(90000);
+(50000,1),
+(1000000,2),
+(250000,3),
+(350000,4),
+(650000,5),
+(190000,6),
+(495000,7),
+(345000,8),
+(2500000,9),
+(190000,10),
+(495000,3),
+(345000,6),
+(90000,1);
 
-insert into overtime_hours(overtime_hours_id)
+insert into overtime_hours(Total_hours_worked)
 values
 (40),
 (39),
@@ -116,4 +130,27 @@ values
 (49),
 (42),
 (21);
+
+ALTER TABLE employees DROP COLUMN department;
+
+
+ALTER TABLE employees DROP COLUMN salary;
+
+
+ALTER TABLE employees DROP COLUMN title;
+
+
+ALTER TABLE employees DROP COLUMN hours_worked;
+
+select employees.first_name, employees.last_name, departments.department_name, titles.titles, salaries.salary, overtime_hours.total_hours_worked
+from employees left join departments
+on employees.department = departments.dep_id
+left join titles
+on employees.title = titles.titles_id
+left join salaries
+on employees.salary = salaries.salaries_id
+left join overtime_hours
+on employees.hours_worked = overtime_hours.overtime_hours_id;
+
+
 
