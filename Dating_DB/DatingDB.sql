b5d1c1c5d2e6fd9de8e6e48da47b9d4de00937b2
KyleWalkerley <78598168+KyleWalkerley@users.noreply.github.com>
2021-03-16 15:32:24 +0200

    Add files via upload

--- Dating_DB/DatingDB.sql
+++ Dating_DB/DatingDB.sql
@@ -6,39 +6,40 @@ phone varchar(10),
 email varchar(100),
 gender varchar (6),
 birthday date,
-CONSTRAINT contact_key PRIMARY KEY (contact_id),
-
+CONSTRAINT contact_key PRIMARY KEY (contact_id)
 );
+
+drop table my_contacts
 
 CREATE TABLE interests(
 interest_id bigserial,
 interest varchar (100),
-CONSTRAINT interest_key PRIMARY KEY (interest_id),
+CONSTRAINT interest_key PRIMARY KEY (interest_id)
 );
 
 CREATE TABLE seeking(
 seeking_id bigserial,
 seeking varchar(50),
-CONSTRAINT seeking_key PRIMARY KEY (seeking_id),
+CONSTRAINT seeking_key PRIMARY KEY (seeking_id)
 );
 
 CREATE TABLE profession(
 prof_id bigserial,
 profession varchar(50),
-CONSTRAINT prof_key PRIMARY KEY (prof_id),
+CONSTRAINT prof_key PRIMARY KEY (prof_id)
 );
 
 CREATE TABLE zip_code(
-zip_code bigserial,
+zip_code_id bigserial,
 city varchar (50),
 province varchar (50),
-CONSTRAINT zip_code_key PRIMARY KEY (zip_code_id),
+CONSTRAINT zip_code_key PRIMARY KEY (zip_code_id)
 );
 
 CREATE TABLE status(
 status_id bigserial,
 status varchar (50),
-CONSTRAINT status_key PRIMARY KEY (status_id),
+CONSTRAINT status_key PRIMARY KEY (status_id)
 );
 
 ALTER TABLE my_contacts
@@ -49,3 +50,42 @@ values
 ('Walkerley','Kyle','0769409564','kyle_walk@icloud.com','male',2001-10-30),
 ('Aartaple','At','0825568794','ATAARTAPLE@gmail.com','male',2001-04-25),
 ('Sak','Sannie','0763254456','SakSannie@internext.co.za','female',1999-12-25);
+
+insert into interests (interest)
+values
+('Loves Cricket'),
+('Loves Swimming'),
+('Loves Bike riding');
+
+insert into seeking (seeking)
+values
+('Wants to meet someone that wants to do the same as me.'),
+('Want to meet someone that can look after me.'),
+('Want to find someone that is passionat about life.');
+
+insert into profession (profession)
+values
+('IT Manager'),
+('Maths Teacher'),
+('Community leader');
+
+insert into zip_code(city,province)
+values
+('Hartbeespoort','North-West'),
+('Cape Town','Eastern Cape'),
+('Balito','KZN');
+
+insert into status(status)
+values
+('Single'),
+('Single'),
+('Divorced');
+
+alter table my_contacts
+add column zip_code integer references zip_code(zip_code_id)
+
+alter table my_contacts
+add column status integer references status (status_id)
+
+alter table my_contacts
+add column profession integer references profession (prof_id)
---
+++ Employees_DB/3.sql
@@ -0,0 +1,153 @@
+CREATE TABLE employees (
+employee_id bigserial,
+first_name varchar(50),
+last_name varchar(50),
+id_number varchar(20),
+age varchar(3),
+nationality varchar(50),
+date_of_birth varchar(50),
+phone varchar(10),
+CONSTRAINT employee_key PRIMARY KEY (employee_id)
+);
+
+drop table employees;
+
+CREATE TABLE departments(
+dep_id bigserial,
+department_name varchar(50),
+CONSTRAINT dep_key PRIMARY KEY (dep_id)
+);
+
+CREATE TABLE salaries(
+salaries_id serial,
+salary numeric,
+CONSTRAINT salaries_key PRIMARY KEY (salaries_id)
+);
+
+drop table salaries;
+
+CREATE TABLE titles(
+titles_id bigserial,
+titles varchar(50),
+CONSTRAINT titles_key PRIMARY KEY (titles_id)	
+);
+
+CREATE TABLE salaries_increase(
+
+);
+
+CREATE TABLE Overtime_Hours(
+overtime_hours_id bigserial,
+Total_hours_worked integer,
+CONSTRAINT overtime_hours_key PRIMARY KEY (overtime_hours_id)
+);
+
+ALTER TABLE employees 
+add column department integer references departments(dep_id);
+
+ALTER TABLE employees 
+add column salary integer references salaries(salaries_id);
+
+ALTER TABLE employees 
+add column titles integer references titles(titles_id);
+
+ALTER TABLE employees 
+add column hours_worked integer references Overtime_Hours(overtime_hours_id);
+
+Insert into employees(first_name,last_name,id_number,age ,nationality,date_of_birth,phone,titles,department,salary,hours_worked)
+Values
+('Kyle', 'Walkerley', '0110305061084', '19', 'South-Africa', 2001-10-30, '0769409564',1,1,1,1),
+('Jan', 'Pan', '0104186452012', '19', 'South-Africa', 2001-04-18, '0845624789',2,2,2,2),
+('Juan', 'Engelbreght', '0109047461325', '19', 'Zimbabwe', 2001-09-04, '0145789354',3,3,3,3),
+('Richard', 'Nel', '0101013458741', '19', 'Botswana', 2001-01-01, '0348789652',4,4,4,4),
+('Robert', 'Spence', '0105097845123', '19', 'Germany', 2001-05-09, '0789561425',5,5,5,5),
+('Stehan', 'Wollmerans', '9906012457987', '21', 'America', 1999-06-01, '0548787451',6,6,6,6),
+('Calvin', 'Smith', '0002013265987', '20', 'South-Africa', 2000-02-01, '0789456123',7,7,7,7),
+('Armin', 'Jonker', '8812258469146', '33', 'Mexico', 1988-12-25, '0767845212',8,8,8,8),
+('Ruan', 'Appelgryn', '0110205784107', '19', 'Japan', 2001-10-20, '0762563254',9,9,9,9),
+('Estian', 'Beeslaar', '0112120201045', '19', 'South-Africa', 2001-12-12, '077894561',10,10,10,10),
+('Lucas', 'Schooman', '0111110245784', '19', 'South-Africa', 2001-11-11, '0789369854',11,11,11,11),
+('Jean-Marie', 'Van der Colf', '0108045487952', '19', 'South-Africa', 2001-08-04, '0014587954',12,12,12,12),
+('Marisca', 'Van der Wald', '0112310125478', '19', 'South-Africa', 2001-12-31, '0894561478',13,13,13,13);
+
+insert into departments (department_name)
+values
+('HR'), 
+('ENGINEERING'), 
+('MANAGEMENT'), 
+('AUTORIZATION'), 
+('MEDICAL_STAFF'), 
+('CLEANERS'), 
+('PRODUCTION'), 
+('PURCHASING'), 
+('ACCOUNTING_FINANCES'), 
+('MARKETING'); 
+
+insert into titles (titles)
+values
+('Manager'),
+('Intern'),
+('Senior'),
+('Junior'),
+('Manager'),
+('Intern'),
+('Senior'),
+('Junior'),
+('Manager'),
+('Senior'),
+('Junior'),
+('Manager'),
+('Intern');
+
+insert into salaries(salary)
+values
+(50000),
+(1000000),
+(250000),
+(350000),
+(650000),
+(190000),
+(495000),
+(345000),
+(2500000),
+(190000),
+(495000),
+(345000),
+(90000);
+
+insert into overtime_hours(Total_hours_worked)
+values
+(40),
+(39),
+(52),
+(50),
+(34),
+(41),
+(40),
+(45),
+(40),
+(40),
+(49),
+(42),
+(21);
+
+ALTER TABLE employees DROP COLUMN department;
+
+
+ALTER TABLE employees DROP COLUMN salary;
+
+
+ALTER TABLE employees DROP COLUMN titles;
+
+
+ALTER TABLE employees DROP COLUMN hours_worked;
+
+select employees.first_name, employees.last_name, departments.department_name, titles.titles, salaries.salary, overtime_hours.total_hours_worked
+from employees left join departments
+on employees.department = departments.dep_id
+left join titles
+on employees.titles = titles.titles_id
+left join salaries
+on employees.salary = salaries.salaries_id
+left join overtime_hours
+on employees.hours_worked = overtime_hours.overtime_hours_id;
